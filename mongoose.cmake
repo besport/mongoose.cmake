84f53862c05fcd9650fb64377ab6a6af27022e26
Hugo Heuzard <hugo.heuzard@gmail.com>
2013-09-09 15:09:24 -0700

    change naming

--- mongoose.cmake
+++ mongoose.cmake
@@ -7,8 +7,8 @@ add_library(mongoose mongoose.c)
 find_package (Threads)
 target_link_libraries (mongoose ${CMAKE_THREAD_LIBS_INIT} ${CMAKE_DL_LIBS})
 
-add_executable(mongoose_test test.c)
-target_link_libraries(mongoose_test mongoose)
+# add_executable(mongoose_test test.c)
+# target_link_libraries(mongoose_test mongoose)
 
 install(TARGETS mongoose DESTINATION lib)
 install(FILES mongoose.h DESTINATION include)
